0903d87ef00ec3b5ceb0b9a042e7ab69d33a7778
Prem Chandu <premchandu7247@gmail.com>
2026-01-29 10:51:40 +0530

    Add CI/CD pipeline for EC2 deployment

--- project1example.py
+++ project1example.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project 1 is LIVE 🚀"
+    return "DevOps Project 1 is LIVE  🚀 prem , it's working so chill dude"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
